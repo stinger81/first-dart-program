06f0da20c3d4d7af44f0fdbe124b5a0c2831b9f9
stinger81 <65797349+stinger81@users.noreply.github.com>
2022-07-05 10:14:42 -0500

    update

--- bin/first_dart_app.dart
+++ bin/first_dart_app.dart
@@ -1,10 +1,7 @@
 import 'package:first_dart_app/first_dart_app.dart' as first_dart_app;
 import 'dart:io';
 void main(List<String> arguments) {
-  //print('Hello world: ${first_dart_app.calculate(4, 6)}!');
-  //sleep(Duration(seconds: 10));
-  //print('Hello world: ${first_dart_app.calculate(18,2)+1}!');
-  //sleep(Duration(seconds: 10));
+
   print("Lets calculate!");
   print("Number 1: ");
   int? n1 = int.parse(stdin.readLineSync()!);
@@ -17,5 +14,5 @@ void main(List<String> arguments) {
     int? n2 = int.parse(stdin.readLineSync()!);
     print("Result: ${first_dart_app.calculate(n1, operation,n2)}");
   }
-  //sleep(Duration(seconds: 10));
+  sleep(Duration(seconds: 10));
 }
